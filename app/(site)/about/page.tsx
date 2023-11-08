3e8a922ca852f111c18539c6f95d5a1efe626bbc
szy her <szymonhernik123@gmail.com>
2023-11-08 14:47:10 +0100

    small project cleaning

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -6,11 +6,13 @@ type Props = {
 }
 
 export default async function Page({ params }: Props) {
-  const page = await getPage(params.slug)
-  console.log(`This is params slug no: ${params.slug}`)
+  const page = await getPage('about')
+  //   console.log(`This is params slug no: ${params.slug}`)
 
   return (
     <div className='pt-12 mx-auto  w-full flex-col items-center lg:w-4/5'>
+      <h1>{page.title}</h1>
+      <h1>{page.title}</h1>
       <h1>{page.title}</h1>
       <PortableText value={page.content}></PortableText>
     </div>
--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,9 +1,10 @@
 import { Layout } from '@/components/dom/Layout'
+import NavLinks from '@/components/dom/nav-links'
 import '@/global.css'
 import { getPages } from '@root/sanity/sanity-utils'
 import Link from 'next/link'
 
-export const revalidate = 60 // revalidate at most every minute
+export const revalidate = 5 // revalidate at most every minute
 
 export const metadata = {
   title: 'Name of the site',
@@ -24,16 +25,13 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       <body className='bg-indigo-200'>
         {/* To avoid FOUT with styled-components wrap Layout with StyledComponentsRegistry https://beta.nextjs.org/docs/styling/css-in-js#styled-components */}
         <header className='py-4 px-8 fixed z-20 flex items-center justify-between w-full'>
-          <Link href='/' className='text-amber-700 '>
+          {/* <Link href='/' className='text-amber-700 '>
             +++
-          </Link>
-          <div className='flex gap-5'>
-            {pages.map((page) => (
-              <Link key={page._id} href={`/${page.slug}`} className=''>
-                {page.title}
-              </Link>
-            ))}
-          </div>
+          </Link> */}
+          <NavLinks />
+          {/* <div className='flex gap-5'>
+            <Link href='/about'>about</Link>
+          </div> */}
         </header>
         <Layout>{children}</Layout>
       </body>
