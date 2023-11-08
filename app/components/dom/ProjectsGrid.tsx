2d808665c83b79fb84675cb6d574c3b98d052bce
szy her <szymonhernik123@gmail.com>
2023-11-08 16:47:56 +0100

    images in a grid on hover

--- app/components/dom/ProjectsGrid.tsx
+++ app/components/dom/ProjectsGrid.tsx
@@ -7,14 +7,20 @@ export default async function ProjectsGrid() {
   return (
     <>
       <section className='w-5/5 flex-col items-center mx-4 mt-36  max-w-screen-2xl 2xl:mx-auto'>
-        <div className='grid grid-cols-4 gap-0 w-full h-[calc(100vh-9rem)] bg-rose-200'>
+        <div className='grid grid-cols-4 gap-0 w-full h-[calc(100vh-9rem)] bg-rose-200 '>
           {projects.map((project) => (
             <Link href={`/projects/${project.slug}`} key={project._id}>
-              <div className='bg-slate-200 relative h-full'>
+              <div className='bg-slate-200 relative h-full group'>
                 <p className=' w-full text-2xl'>{project.name}</p>
                 <div className='mt-4 flex-col p-4 absolute top-0 left-0'>
                   {project.image && (
-                    <Image src={project.image} alt={project.name} width={250} height={100} className='object-cover' />
+                    <Image
+                      src={project.image}
+                      alt={project.name}
+                      width={250}
+                      height={100}
+                      className='object-cover opacity-0 group-hover:opacity-100 transition-opacity'
+                    />
                   )}
                 </div>
               </div>
