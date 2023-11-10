9a8e5c167d683ea912c9b0bd801646f1471ba5fb
szy her <szymonhernik123@gmail.com>
2023-11-10 10:14:38 +0100

    checking card skeleton

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,12 +3,13 @@ import Image from 'next/image'
 import Link from 'next/link'
 import ProjectsGrid from '@/components/dom/ProjectsGrid'
 import { Suspense } from 'react'
+import { CardSkeleton } from '@/components/canvas/CardSkeleton'
 
 export default async function Page() {
   const projects = await getProjects()
   return (
     <>
-      <Suspense>
+      <Suspense fallback={<CardSkeleton />}>
         <ProjectsGrid />
       </Suspense>
       {/* <div className='w-full text-center md:w-3/5'>
